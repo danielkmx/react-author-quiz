2564cffc86a1ad6b94344ff90ccf11c0e731ec26
Daniel bezerra <danielbezerrakmx@gmail.com>
2018-09-08 18:07:54 -0300

    Adding AuthorQuiz components and tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import {shuffle,sample} from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/Mark_twain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn', 'The French Revolution' , 'King Arthur', 'Arabian Nights']
+    },
+    {
+        name: 'ERNEST HEMINGWAY',
+        imageUrl: 'images/ernest_hemingway.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Anna Karenina', 'Far away Long Ago', 'BuddenBrooks,Madame Bovary']
+    },
+    {
+        name: 'ERIK LARSON',
+        imageUrl: 'images/erik_larson.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['In the Garden of beats',  'The Devil in the White City', 'Thunderstruck, Lethal Passage']
+    },
+    {
+        name: ' F. SCOTT FITZGERALD',
+        imageUrl: 'images/scott_fitzgerald.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Cray Sunday', 'A new Leaf, The offshore Pirate', 'Bernice Bobs Her hair']
+    }
+]
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function(p,c,i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const awnser = sample(fourRandomBooks);
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+        author.books.some((title) => 
+        title === awnser ))
+    }
+}
+const state = {
+    turnData: getTurnData(authors),
+    highlight: 'white'
+}
+function onAnswerSelected(answer){
+const isCorrect = state.turnData.author.books.some((book) => book === answer)
+state.highlight = isCorrect ? 'correct' : 'wrong';
+render();
+}
+function render(){
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+
+}
+render();
 registerServiceWorker();
+
